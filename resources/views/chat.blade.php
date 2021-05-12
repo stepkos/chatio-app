728fede077663709dbac23beb9a23ee2a38f9d6e
stepkos <stepkos@wp.pl>
2021-05-12 15:09:08 +0200

    new date in views

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -32,12 +32,11 @@
                         </div>
 
                         <div class="info">
-                            <h2>Kamil Paczkowski</h2>
-                            <h3>AC/DC Page</h3>
+                            <h2>Kacper Nowak</h2>
+                            <h3>Project Manager</h3>
                         </div>
 
-                        <!-- status - zielona kropka jak jestes wlascicielem projektu -->
-                        <div class="owner"></div>
+                        <div class="active"></div>
 
                     </div>
 
@@ -48,8 +47,21 @@
                         </div>
 
                         <div class="info">
-                            <h2>Jan Napieralski</h2>
-                            <h3>Hackaton 2021</h3>
+                            <h2>Patryk Okrasa</h2>
+                            <h3>Back-End Developer</h3>
+                        </div>
+
+                    </div>
+
+                    <div class="person">
+
+                        <div>
+                            <img src="{{ asset('images/pitt.jpg')}}" alt="profile picture">
+                        </div>
+
+                        <div class="info">
+                            <h2>Adam Słowiński</h2>
+                            <h3>Marketing Manager</h3>
                         </div>
 
                     </div>
@@ -67,51 +79,49 @@
                     </div>
 
                     <div class="info">
-                        <h2>Kamil Paczkowski</h2>
-                        <h3>AC/DC Page</h3>
+                        <h2>Kacper Nowak</h2>
+                        <h3>Project Manager</h3>
                     </div>
 
-                    <div class="owner"></div>
+                    <div class="active"></div>
                 </header>
 
                 <div class="chat">
 
-                    <div class="sent">Siema</div>
+                    <div class="sent">Lorem</div>
                     <div style="clear: both;"></div>
 
-                    <div class="sent">Ciekawe kto wpadał na tak szalony pomysł by pisać tą apkę w PHP</div>
+                    <div class="sent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quam!</div>
                     <div style="clear: both;"></div>
 
-                    <div class="received">Elo</div>
+                    <div class="received">Adipisicing elit</div>
                     <div style="clear: both;"></div>
 
-                    <div class="received">No przecież nie kto inny jak Ty mordziaty XD</div>
+                    <div class="received">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut explicabo, neque voluptate unde rerum et non reiciendis amet. Quidem, libero.</div>
                     <div style="clear: both;"></div>
 
-                    <div class="sent">Postaram się do rana front-end do tech chatu zrobić</div>
+                    <div class="sent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, corporis.</div>
                     <div style="clear: both;"></div>
 
-                    <div class="received">Dooobra już nie mogę sie doczekać. Piszę teraz jaka dłuższa wiadomość tylko po to by zrobić test ale z tego co widzę zrobiłem to genialnie!</div>
+                    <div class="received">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque maiores soluta saepe excepturi reprehenderit accusamus necessitatibus id praesentium minus eum illum, ea incidunt commodi dolorem quis modi officia beatae asperiores dicta ipsum.</div>
                     <div style="clear: both;"></div>
 
-
-
-                    <div class="sent">Siema</div>
+                    <div class="sent">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus rerum quasi placeat?</div>
                     <div style="clear: both;"></div>
 
-                    <div class="sent">Ciekawe kto wpadał na tak szalony pomysł by pisać tą apkę w PHP</div>
+                    <div class="sent">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
                     <div style="clear: both;"></div>
 
-                    <div class="received">Elo</div>
+                    <div class="received">Sit amet</div>
                     <div style="clear: both;"></div>
 
-                    <div class="received">No przecież nie kto inny jak Ty mordziaty XD</div>
+                    <div class="received">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ipsam exercitationem expedita magnam veritatis fuga cum debitis consectetur!</div>
                     <div style="clear: both;"></div>
 
-                    <div class="sent">Postaram się do rana front-end do tech chatu zrobić</div>
+                    <div class="sent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium aspernatur vitae explicabo architecto molestiae qui quidem pariatur nesciunt laboriosam facere?</div>
                     <div style="clear: both;"></div>
 
-                    <div class="received">Dooobra już nie mogę sie doczekać. Piszę teraz jaka dłuższa wiadomość tylko po to by zrobić test ale z tego co widzę zrobiłem to genialnie!</div>
+                    <div class="received">Lorem ipsum dolor sit amet consectetur.</div>
                     <div style="clear: both;"></div>
 
                 </div>
